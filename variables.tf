5f6048cf190315e11684ec9a1956b97348d55222
Sameer-Sarrainodu <sameer339977.com>
2025-10-31 10:33:53 +0530

    ci-cdtools

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+    default = "roboshop"
+
+}
+variable "environment" {
+    default = "dev"
+
+}
+variable "zone_id" {
+    default = "Z0022572U6LHZ3ASAGBB"
+
+}
+variable "zone_name" {
+    default = "sharkdev.shop"
+
+}
